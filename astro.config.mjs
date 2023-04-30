35ab90d0974f0eec70002a56b41c3b9e579444a0
Kacper Wyczawski <wyczawskikacper@gmail.com>
2023-04-30 15:05:39 +0200

    add prefetch

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
+import prefetch from "@astrojs/prefetch";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), prefetch({
+    selector: "a[href^='/']", // prefetch links that start with "/"
+  })],
   site: "https://kacperwyczawski.github.io",
-  base: "/",
+  base: "/"
 });
