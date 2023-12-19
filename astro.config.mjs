7c7f089d2be7bfe860b73012968b0c6cce44498d
Kacper Wyczawski <kacper@wyczawski.dev>
2023-12-19 16:58:12 +0100

    refactor: migrate to astro v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,10 @@
 import { defineConfig, squooshImageService } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
-import prefetch from "@astrojs/prefetch";
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), prefetch({
-    selector: "a[href^='/']", // prefetch links that start with "/"
-  })],
+  integrations: [
+    tailwind()
+  ],
   image: {
     service: squooshImageService()
   },
