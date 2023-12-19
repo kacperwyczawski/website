bf1737ace02d9aa5d0a39b8a1e25c62738feb5bb
Kacper Wyczawski <kacper@wyczawski.dev>
2023-12-19 19:25:00 +0100

    feat: rework content collections

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,4 +1,4 @@
-import { z, defineCollection} from "astro:content";
+import { z, defineCollection } from "astro:content";
 
 const definitionsCollection = defineCollection({
     type: "data",
@@ -48,6 +48,58 @@ const definitionsCollection = defineCollection({
     })
 });
 
+const indexCollection = defineCollection({
+    type: "content",
+    schema: z.object({
+        links: z.array(z.object({
+            icon: z.string(),
+            name: z.string(),
+            url: z.string().url()
+        })),
+    })
+})
+const projectsCollection = defineCollection({
+    type: "data",
+    schema: z.object({
+        name: z.string(),
+        status: z.string().optional(),
+        technologies: z.array(z.string()),
+        description: z.string(),
+        sourceUrl: z.string().url().optional(),
+        liveUrl: z.string().url().optional(),
+        imagePath: z.string().optional(),
+    })
+})
+const resumeCollection = defineCollection({
+    type: "data",
+    schema: z.object({
+        skills: z.array(z.object({
+            icon: z.string(),
+            title: z.string(),
+            preferred: z.array(z.string()),
+            other: z.array(z.string())
+        })),
+        workExperience: z.array(z.object({
+            date: z.string(),
+            location: z.string(),
+            type: z.string(),
+            title: z.string(),
+            companyName: z.string(),
+            companyUrl: z.string().url(),
+        })),
+        education: z.array(z.object({
+            date: z.string(),
+            location: z.string(),
+            title: z.string(),
+            schoolName: z.string(),
+            schoolUrl: z.string().url(),
+        })),
+    })
+})
+
 export const collections = {
     "definitions": definitionsCollection,
+    "index": indexCollection,
+    "projects": projectsCollection,
+    "resume": resumeCollection,
 }
