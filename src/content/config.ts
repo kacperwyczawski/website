50594d68c24e23a1179784c649dbda2eeaefaf26
Kacper Wyczawski <kacper@wyczawski.dev>
2024-03-05 17:20:55 +0100

    refactor: separate links from index text in content collections

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,76 +1,17 @@
 import { z, defineCollection } from "astro:content";
 
-const definitionsCollection = defineCollection({
-  type: "data",
-  schema: z.object({
-    socialLinks: z.object({
-      print: z.array(
-        z.object({
-          icon: z.string(),
-          text: z.string(),
-        }),
-      ),
-      web: z.array(
-        z.object({
-          icon: z.string(),
-          text: z.string(),
-          url: z.string().url(),
-        }),
-      ),
-    }),
-    aboutMe: z.string(),
-    skills: z.array(
-      z.object({
-        icon: z.string(),
-        title: z.string(),
-        preferred: z.array(z.string()),
-        other: z.array(z.string()),
-      }),
-    ),
-    workExperience: z.array(
-      z.object({
-        date: z.string(),
-        location: z.string(),
-        type: z.string(),
-        title: z.string(),
-        company: z.string(),
-        companyUrl: z.string().url(),
-      }),
-    ),
-    education: z.array(
-      z.object({
-        date: z.string(),
-        location: z.string(),
-        title: z.string(),
-        school: z.string(),
-        schoolUrl: z.string().url(),
-      }),
-    ),
-    projects: z.array(
-      z.object({
-        name: z.string(),
-        status: z.string().optional(),
-        technologies: z.array(z.string()),
-        description: z.string(),
-        sourceUrl: z.string().url().optional(),
-        liveUrl: z.string().url().optional(),
-        imagePath: z.string().optional(),
-      }),
-    ),
-  }),
-});
-
 const indexCollection = defineCollection({
   type: "content",
-  schema: z.object({
-    links: z.array(
-      z.object({
-        icon: z.string(),
-        name: z.string(),
-        url: z.string().url(),
-      }),
-    ),
-  }),
+});
+const linksCollection = defineCollection({
+  type: "data",
+  schema: z.array(
+    z.object({
+      icon: z.string(),
+      name: z.string(),
+      url: z.string().url(),
+    }),
+  ),
 });
 const projectsCollection = defineCollection({
   type: "data",
@@ -119,8 +60,8 @@ const resumeCollection = defineCollection({
 });
 
 export const collections = {
-  definitions: definitionsCollection,
   index: indexCollection,
+  links: linksCollection,
   projects: projectsCollection,
   resume: resumeCollection,
 };
