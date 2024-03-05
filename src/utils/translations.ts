e86bc9ff722803b0c6c440fce92396e21e0e5dd3
Kacper Wyczawski <kacper@wyczawski.dev>
2024-03-05 20:15:44 +0100

    feat: translate navbar

---
+++ src/utils/translations.ts
@@ -0,0 +1,18 @@
+export const translations = {
+    'nav.projects': {
+        pl: 'Projekty',
+        en: 'Projects'
+    },
+    'nav.resume': {
+        pl: 'CV',
+        en: 'Resume'
+    },
+    'section.about': {
+        pl: 'O mnie',
+        en: 'About me'
+    },
+}
+
+export function translate(key: keyof typeof translations, lang: string | undefined) {
+    return translations[key][lang as 'pl' ?? 'en'];
+}
