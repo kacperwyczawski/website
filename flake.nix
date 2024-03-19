162002ed3db1cffb52f2da894def9e897eb2093a
Kacper Wyczawski <kacper@wyczawski.dev>
2024-03-19 16:01:41 +0100

    refactor: flake.nix

--- flake.nix
+++ flake.nix
@@ -1,24 +1,17 @@
 {
   description = "Kacper Wyczawski's website";
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-  };
-  outputs = { nixpkgs ,... }: let
-    system = "x86_64-linux";
-  in {
-    devShells."${system}".default = let
-      pkgs = import nixpkgs {
-        inherit system;
-      };
-    in pkgs.mkShell {
-      packages = with pkgs; [
-        bun
-        biome
-        nushell
-      ];
-      shellHook = ''
-        exec nu -e 'bun dev'
-      '';
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; };
+  outputs = { nixpkgs, ... }:
+    let system = "x86_64-linux";
+    in {
+      formatter."${system}" = nixpkgs.legacyPackages.${system}.nixfmt;
+      devShells."${system}".default =
+        let pkgs = nixpkgs.legacyPackages.${system};
+        in pkgs.mkShell {
+          packages = with pkgs; [ bun biome nushell ];
+          shellHook = ''
+            exec nu -e 'bun dev'
+          '';
+        };
     };
-  };
 }
