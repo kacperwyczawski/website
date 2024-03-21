20d84dd009811321d61b08bde570739e14670e9a
Kacper Wyczawski <kacper@wyczawski.dev>
2024-03-21 15:32:22 +0100

    refactor: flake.nix

--- flake.nix
+++ flake.nix
@@ -4,14 +4,13 @@
   outputs = { nixpkgs, ... }:
     let system = "x86_64-linux";
     in {
-      formatter."${system}" = nixpkgs.legacyPackages.${system}.nixfmt;
-      devShells."${system}".default =
-        let pkgs = nixpkgs.legacyPackages.${system};
-        in pkgs.mkShell {
-          packages = with pkgs; [ bun biome nushell ];
-          shellHook = ''
-            exec nu -e 'bun dev'
-          '';
-        };
+      formatter.${system} = nixpkgs.legacyPackages.${system}.nixfmt;
+      devShells.${system}.default = let pkgs = nixpkgs.legacyPackages.${system};
+      in pkgs.mkShell {
+        packages = with pkgs; [ bun biome nushell ];
+        shellHook = ''
+          exec nu -e 'bun dev'
+        '';
+      };
     };
 }
