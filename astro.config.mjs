c617eff7b2afd21ac792d620700829b3fe9416ef
Kacper Wyczawski <kacper@wyczawski.dev>
2024-09-22 17:20:15 +0200

    feat: update code and dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,27 @@
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import { defineConfig, squooshImageService } from "astro/config";
+import { defineConfig, passthroughImageService } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+	site: "https://wyczawski.dev",
 	integrations: [
 		tailwind(),
 		sitemap({
-			customPages: ["https://autocut.wyczawski.dev/"],
+			customPages: [
+				"https://autocut.wyczawski.dev/",
+				"https://arx.wyczawski.dev",
+			],
+			i18n: {
+				defaultLocale: "en",
+				locales: {
+					pl: "pl-PL",
+				}
+			}
 		}),
 	],
 	image: {
-		service: squooshImageService(),
+		service: passthroughImageService(),
 	},
 	devToolbar: {
 		enabled: false,
--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -1,14 +1,14 @@
 import type { APIRoute } from "astro";
 
-const robotsTxt = `
+const getRobotsTxt = (sitemapURL: URL) => `
 User-agent: *
 Allow: /
-Disallow: /cdn-cgi/
-Sitemap: ${new URL("sitemap-index.xml", import.meta.env.SITE).href}
-`.trim();
 
-export const GET: APIRoute = () => {
-	return new Response(robotsTxt, {
+Sitemap: ${sitemapURL.href}
+`;
+
+export const GET: APIRoute = ({ site }) => {
+	return new Response(getRobotsTxt(new URL('sitemap-index.xml', site)), {
 		headers: {
 			"Content-Type": "text/plain; charset=utf-8",
 		},
