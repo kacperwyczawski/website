2935930d3e55abe35d4d18eb3a563d71e123b491
Kacper Wyczawski <kacper@wyczawski.dev>
2025-01-10 19:44:41 +0100

    fix: url in sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ export default defineConfig({
 		sitemap({
 			customPages: [
 				"https://autocut.wyczawski.dev/",
-				"https://arx.wyczawski.dev",
+				"https://arx.wyczawski.dev/",
 			],
 			i18n: {
 				defaultLocale: "en",
