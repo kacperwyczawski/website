a9208005f16a380c9b6d80e886ce326b31133482
Kacper Wyczawski <kacper@wyczawski.dev>
2025-01-29 19:11:33 +0100

    feat: migrate to tailwind v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
 import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig, passthroughImageService } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://wyczawski.dev",
+	vite: {
+		plugins: [
+			tailwindcss()
+		],
+	},
 	integrations: [
-		tailwind(),
 		sitemap({
 			customPages: [
 				"https://autocut.wyczawski.dev/",
